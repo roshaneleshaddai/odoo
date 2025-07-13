d8c65fb164cefb5da64b466e399f1758620cb617
roshaneleshaddai <138379526+roshaneleshaddai@users.noreply.github.com>
2025-07-13 17:35:45 +0530

    Update api.js

--- frontend/utils/api.js
+++ frontend/utils/api.js
@@ -1,7 +1,7 @@
 import axios from 'axios';
 
 export const api = axios.create({
-  baseURL: 'https://odoo-5534.onrender.com/ap',
+  baseURL: 'https://odoo-5534.onrender.com/api',
   headers: {
     'Content-Type': 'application/json',
   },
